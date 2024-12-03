788088ffd812a08d6cde9e19ddb45c4c129a1e0d
Mundane-Editorial <uday2k17@rediffmail.com>
2024-12-03 21:02:22 +0530

    added router package, removed the navigation naming errors in App.js and few minor fixes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import './App.css'; // Import custom styles
 import React from 'react';
-import Navbar from './Components/Navbar';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Components/navbar';
+import Home from './Pages/Home'; // Example page
+import Services from './Pages/Services'; // Example page
+import Products from './Pages/Products'; // Example page
+import Contactme from './Pages/Contactme'; // Correct path
+import AboutUs from './Pages/Aboutus';
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About-us" element={<AboutUs />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/contactme" element={<Contactme />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/Pages/Aboutus.js
+++ src/Pages/Aboutus.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Aboutus = () => {
+const AboutUs = () => {
   return (
     <div>BRUHHHHHHHHHHH!!!!!!</div>
   )
 }
 
-export default Aboutus
+export default AboutUs
